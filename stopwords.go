0fafac29079c528407ed2393fdda28951744e908
Gildas Chabot <gildas.chabot@scmfrance.fr>
2016-02-16 13:54:59 +0100

    Allowing for digits

--- stopwords.go
+++ stopwords.go
@@ -25,7 +25,7 @@ import (
 var (
 	remTags      = regexp.MustCompile(`<[^>]*>`)
 	oneSpace     = regexp.MustCompile(`\s{2,}`)
-	unicodeWords = regexp.MustCompile(`[\pL\p{Mc}\p{Mn}-_']+`)
+	unicodeWords = regexp.MustCompile(`[\pL0-9\p{Mc}\p{Mn}-_']+`)
 )
 
 // CleanString removes useless spaces and stop words from string content.
